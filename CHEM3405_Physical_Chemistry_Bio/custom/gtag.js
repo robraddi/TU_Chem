9b72046f80c6cfe40b47d68f0f2e12cc376ffe9e
Rob Raddi <rraddi@temple.edu>
2019-08-28 14:25:34 -0400

    adding new course

---
+++ CHEM3405_Physical_Chemistry_Bio/custom/gtag.js
@@ -0,0 +1,10 @@
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114056594-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-114056594-3');
+</script>
+
